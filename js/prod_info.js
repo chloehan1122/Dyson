35ad654050c089490a25248fe15dda755526392b
chloehan <hanj951122@gmail.com>
2024-06-27 18:23:59 +0900

    update

--- js/prod_info.js
+++ js/prod_info.js
@@ -1,11 +1,24 @@
 let scrTop = 0;
+let prodPrice = ""
 $(document).ready(function(){
 
-
+// price
+prodPrice = [
+    {
+        name: "솔라사이클모프조명 데스크형",
+        price: "₩720,000"
+    },
+    {
+        name: "솔라사이클모프조명 플로어 스탠드형",
+        price: "₩960,000"
+    }
+]
     // 제품 종류 - 탭메뉴
-    $(".dl_type>dt").click(function(){
+    $(".dl_type>dt").click(function(index){
         $(".dl_type>dt").removeClass("on")
         $(this).addClass("on")
+        let activeIdx = $(this).index() 
+        console.log(activeIdx)
     })
 
     // 제품 색상 - 슬라이드
@@ -17,9 +30,6 @@ $(document).ready(function(){
         $(".img_color>li").removeClass("on")
         $(".img_color>li").eq(btn_idx).addClass("on")
     })
-
-
-
 
 
 
